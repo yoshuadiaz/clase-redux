a6fc4c89c741975f14c77ac749c8c036f6734836
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2019-06-15 10:55:23 -0500

    First Store

--- src/index.js
+++ src/index.js
@@ -3,18 +3,42 @@ import ReactDOM from "react-dom"
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import Counter from "./pages/Counter"
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+
+const initialState = {
+  counter: 0,
+  menssage: "Coman frutas y verduras"
+}
+function CounterReducer(state = initialState, action) {
+  switch (action.type) {
+    case "sumar uno":
+      return { ...state, counter: state.counter + 1 }
+    case "restar uno":
+      return { ...state, counter: state.counter - 1 }
+    default:
+      return state
+  }
+}
+
+const store = createStore(
+  CounterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/contador" component={Counter} />
-      <Route>
-        <h1>404</h1>
-        <p>¡¡¡Eso ni existe!!!</p>
-        <p>Te voy a acusar con mi mamá</p>
-      </Route>
-    </Switch>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/contador" component={Counter} />
+        <Route>
+          <h1>404</h1>
+          <p>¡¡¡Eso ni existe!!!</p>
+          <p>Te voy a acusar con mi mamá</p>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 )
